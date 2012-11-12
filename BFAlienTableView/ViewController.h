c6925667f58fb4fe8398e1f33a7c5370061545b0
Dario Lencina <darioalessandro@me.com>
2012-11-11 19:07:52 -0500

    Initial Commit

---
+++ BFAlienTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BFAlienTableView
+//
+//  Created by Dario Lencina on 11/11/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BFAlienTableView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BFAlienTableView
+//
+//  Created by Dario Lencina on 11/11/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
